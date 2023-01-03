b1fec33dbbcd3d44149a2d1a12abd0b76f64da83
Romulo Menezes <romulo.m.menezes@hotmail.com>
2023-01-03 14:40:51 -0300

    feat: adiciona tabela de videos

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Video from './Video'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -27,8 +28,11 @@ export default class User extends BaseModel {
   @column()
   public last_name: string
 
+  @hasMany(() => Video)
+  public videos: HasMany<typeof Video>
+
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
