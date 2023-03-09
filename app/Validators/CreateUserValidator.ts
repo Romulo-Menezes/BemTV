e725e9639907fa40cab1f34faada2cb3c511f6dc
Romulo Menezes <romulo.m.menezes@hotmail.com>
2023-03-08 23:21:50 -0300

    feat: permite nomes com acentuações

--- app/Validators/CreateUserValidator.ts
+++ app/Validators/CreateUserValidator.ts
@@ -9,9 +9,14 @@ export default class CreateUserValidator {
       rules.maxLength(32),
       rules.trim(),
       rules.escape(),
-      rules.alpha(),
+      rules.regex(/^[a-zA-ZÀ-ÿ]$/),
     ]),
-    lastName: schema.string({}, [rules.maxLength(32), rules.trim(), rules.escape(), rules.alpha()]),
+    lastName: schema.string({}, [
+      rules.maxLength(32),
+      rules.trim(),
+      rules.escape(),
+      rules.regex(/^[a-zA-ZÀ-ÿ]$/),
+    ]),
     email: schema.string({}, [
       rules.email(),
       rules.normalizeEmail({
@@ -31,10 +36,10 @@ export default class CreateUserValidator {
 
   public messages: CustomMessages = {
     'firstName.required': 'Seu nome é obrigatório para o cadastro!',
-    'firstName.alpha': 'Seu nome deve conter apenas letras!',
+    'firstName.regex': 'Seu nome deve conter apenas letras!',
     'firstName.maxLength': 'O tamanho máximo de um nome é de 32 caracteres.',
     'lastName.required': 'Seu sobrenome é obrigatório para o cadastro!',
-    'lastName.alpha': 'Seu sobrenome deve conter apenas letras!',
+    'lastName.regex': 'Seu sobrenome deve conter apenas letras!',
     'lastName.maxLength': 'O tamanho máximo de um sobrenome é de 32 caracteres.',
     'email.required': 'Seu e-mail é obrigatório para o cadastro!',
     'email.unique': 'Endereço de e-mail já cadastrado!',
